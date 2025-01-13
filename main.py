d8b2d0a579166133dcf214776be3560f340f5c91
alexrss85 <2023_alex.rosendo@iticbcn.cat>
2025-01-13 18:01:46 +0100

    exercici 2 acabat

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from connection import conexio
 
 conn = conexio()
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+class User(BaseModel):
+    nombre: str    # Alex
+    apellido: str   # Rosendo
+    correoElectronico: str  # a@gmail.com
+    descripcion: str | None = None # hola
+    curso: str  # DAW
+    ano: int    # 2
+    direccion: str  # Carrer itic
+    codigoPostal: int | None = None #11111111
+    password: str   #12345Ab
+
+async def add_user(nombre: str, apellido: str, correoElectronico: int, descripcion: str, curso: str,):
+  
+    
+
+
+
+
+
